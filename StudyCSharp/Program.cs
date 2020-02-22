010ba0cdb128159850fdfa9e40ecdb20a0d75219
linson <lsfv@163.com>
2020-02-22 08:51:21 +0800

    ok.

--- StudyCSharp/Program.cs
+++ StudyCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace StudyCSharp
     {
         static void Main(string[] args)
         {
+            DataTable dataTable = Library.TestData.GetDatatable();
+            dataTable.Rows.AsQueryable();
         }
     }
 }
---
+++ library/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public abstract class TestData
+    {
+        public static DataTable GetDatatable()
+        {
+            DataTable databable_books = new DataTable("Books");
+
+
+            DataColumn tempDataColumn = null;
+            tempDataColumn = databable_books.Columns.Add("ID", Type.GetType("System.Int32"));
+            tempDataColumn.AutoIncrement = true;//自动增加
+            tempDataColumn.AutoIncrementSeed = 1;//起始为1
+            tempDataColumn.AutoIncrementStep = 1;//步长为1
+            tempDataColumn.AllowDBNull = false;//
+
+            databable_books.Columns.Add("Product", Type.GetType("System.String"));
+            databable_books.Columns.Add("BookID", Type.GetType("System.Int32"));
+            databable_books.Columns.Add("Description", Type.GetType("System.String"));
+
+            DataRow newRow;
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "c++";
+            newRow["BookID"] = 1;
+            newRow["Description"] = "我很喜欢";
+            databable_books.Rows.Add(newRow);
+
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "c#";
+            newRow["BookID"] = 2;
+            newRow["Description"] = "very nice";
+            databable_books.Rows.Add(newRow);
+
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "java";
+            newRow["BookID"] = 3;
+            newRow["Description"] = "good id";
+            databable_books.Rows.Add(newRow);
+
+            return databable_books;
+        }
+
+        public static DataTable GetDatatable2()
+        {
+            DataTable databable_books = new DataTable("Books");
+
+
+            DataColumn tempDataColumn = null;
+            tempDataColumn = databable_books.Columns.Add("ID", Type.GetType("System.Int32"));
+            tempDataColumn.AutoIncrement = true;//自动增加
+            tempDataColumn.AutoIncrementSeed = 1;//起始为1
+            tempDataColumn.AutoIncrementStep = 1;//步长为1
+            tempDataColumn.AllowDBNull = false;//
+
+            databable_books.Columns.Add("Product", Type.GetType("System.String"));
+            databable_books.Columns.Add("BookID", Type.GetType("System.Int32"));
+            databable_books.Columns.Add("Description", Type.GetType("System.String"));
+
+            DataRow newRow;
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "c++";
+            newRow["BookID"] = 1;
+            newRow["Description"] = "我很喜欢1";
+            databable_books.Rows.Add(newRow);
+
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "c#";
+            newRow["BookID"] = 2;
+            newRow["Description"] = "very nice1";
+            databable_books.Rows.Add(newRow);
+
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "java";
+            newRow["BookID"] = 3;
+            newRow["Description"] = "good id1";
+            databable_books.Rows.Add(newRow);
+
+            newRow = databable_books.NewRow();
+            newRow["Product"] = "js";
+            newRow["BookID"] = 4;
+            newRow["Description"] = "good id1";
+            databable_books.Rows.Add(newRow);
+
+            return databable_books;
+        }
+    }
+}
